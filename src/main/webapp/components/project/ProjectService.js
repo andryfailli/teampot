afe3a82db347963def61185eafe4dfa3d940d258
Andrea Failli <andry.failli@gmail.com>
2014-10-08 17:12:40 +0200

    Project entity service

--- src/main/webapp/components/project/ProjectService.js
+++ src/main/webapp/components/project/ProjectService.js
@@ -1,11 +1,26 @@
 angular.module("teampot").
-	factory('ProjectService',function() {
-		return {
-			$new: function(){
-				return {
-					//TODO: to be implemented
-					$save: function(){}
+	factory('ProjectService',function(GapiClient) {
+		
+		client = GapiClient.client("teampot");
+		
+		var service = {
+			$new: function(entity){
+				entity = entity || {}
+				entity.$save = function(){
+					return client.exec("project.save",entity);
 				}
+				return entity;
+			},
+			$list: function(){
+				var entityList = client.exec("project.list");
+				entityList.$promise.then(function(){
+					for (var i=0; i<entityList.items.length; i++) {
+						service.$new(entityList.items[i]);
+					}
+				});
+				return entityList;
 			}
 		};
+		
+		return service;
 	});
