22d2cdb1faa63d9841cec86046bae27763a50d05
Andrea Failli <andry.failli@gmail.com>
2014-10-10 15:29:53 +0200

    task saving

--- src/main/webapp/components/task/edit.js
+++ src/main/webapp/components/task/edit.js
@@ -1,7 +1,11 @@
 angular.module('teampot').
-	controller('taskEditController', function($scope,$routeParams,ProjectService,TaskService) {
+	controller('taskEditController', function($rootScope,$scope,$routeParams,ProjectService,TaskService) {
 		
 		$scope.project = ProjectService.$get($routeParams.projectKey);
-		$scope.task = $routeParams.taskKey ? TaskService.$get($routeParams.taskKey) : TaskService.$new();
+		$scope.task = $routeParams.taskKey ? TaskService.$get($routeParams.taskKey) : TaskService.$new({project:$routeParams.projectKey});
+		
+		$scope.$on("$destroy",function(){
+			$scope.task.$save();
+		});
 		
 	});
