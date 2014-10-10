e4c0cdd632b816c9fb83197307194206d2195812
Andrea Failli <andry.failli@gmail.com>
2014-10-10 15:42:53 +0200

    task edit fromt ask list

--- src/main/webapp/components/task/list.js
+++ src/main/webapp/components/task/list.js
@@ -1,7 +1,11 @@
 angular.module('teampot').
-	controller('taskListController', function($scope,$routeParams,ProjectService,TaskService) {
+	controller('taskListController', function($scope,$routeParams,$location,ProjectService,TaskService) {
 		
 		$scope.project = ProjectService.$get($routeParams.projectKey);
 		$scope.taskList = TaskService.$list();
+		
+		$scope.goto = function(task){
+			$location.path("/project/"+$routeParams.projectKey+"/task/"+task.key);
+		}
 		
 	});
