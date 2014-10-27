75056e2fe67dffe468a70e66141f6e3cf7d19496
Andrea Failli <andry.failli@gmail.com>
2014-10-27 17:12:10 +0100

    project creation loading spinner

--- src/main/webapp/components/project/edit-modal.js
+++ src/main/webapp/components/project/edit-modal.js
@@ -2,9 +2,9 @@ angular.module('teampot').
 	controller('projectEditModalController', function($rootScope,$scope,$materialDialog,targetProject) {
 		
 		$scope.project = targetProject;
-		
+
 		$scope.save = function(){
-			
+			$scope.loading = true;
 			$scope.project.$save().$promise.then(function(){
 				$rootScope.$apply(function(){
 					$materialDialog.hide($scope.project);
