bfa32d91441e178e2479396263e1e76418a1401f
= <=>
2014-11-10 18:18:12 +0100

    meeting list polish

--- src/main/webapp/components/meeting/list.js
+++ src/main/webapp/components/meeting/list.js
@@ -3,6 +3,16 @@ angular.module('teampot').
 		
 		$scope.meetingList = MeetingService.$list($routeParams.projectId);
 		
+		$scope.filter_isToBeScheduled = function(meeting) {
+			return !meeting.timestamp;
+		}	
+		$scope.filter_isPast = function(meeting) {
+			return meeting.timestamp && meeting.past;
+		}
+		$scope.filter_isNotPast = function(meeting) {
+			return meeting.timestamp && !meeting.past;
+		}
+
 		$scope.goto = function(meeting){
 			$location.path("/project/"+$routeParams.projectId+"/meeting/"+meeting.id);
 		}
