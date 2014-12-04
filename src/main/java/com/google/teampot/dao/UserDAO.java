a312ba83e6cd56f40f849d66ebf56c0ee46bbfe5
= <=>
2014-12-04 18:51:37 +0100

    hotfix

--- src/main/java/com/google/teampot/dao/UserDAO.java
+++ src/main/java/com/google/teampot/dao/UserDAO.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
-import org.apache.commons.collections.ListUtils;
-
 import com.google.teampot.model.User;
 
 import static com.google.teampot.OfyService.ofy;
