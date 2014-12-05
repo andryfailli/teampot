ef3b84269e761ab66e37b963274cb99951fd479c
= <=>
2014-12-05 09:24:07 +0100

    added typeahead in members-add

--- src/main/webapp/components/user/add-modal.js
+++ src/main/webapp/components/user/add-modal.js
@@ -1,29 +1,33 @@
 angular.module('teampot').
-	controller('userAddModalController', function($rootScope,$scope,$mdDialog,$routeParams,GapiClient,NotifyService,$route) {
+	controller('userAddModalController', function($rootScope,$scope,$mdDialog,$routeParams,GapiClient,NotifyService,$route,UserService) {
 		
 		$scope.projectId = $routeParams.projectId;
+		
+		$scope.lookupUsers = function(q){
+			return q ? UserService.$list({q:q}) : UserService.$list();
+		}
 
 		$scope.add = function(){
 			
 			var newUser = GapiClient.client("teampot").exec("project.addMember",{
 				id: $routeParams.projectId,
-				memberEmail: $scope.userEmail
+				memberEmail: $scope.user.email
 			});
 			
-			NotifyService.info("Adding user "+$scope.userEmail+" ...");
+			NotifyService.info("Adding user "+$scope.user.email+" ...");
 			$mdDialog.hide(newUser);
 			
 			newUser.$promise
 				.then(function(){
 					$rootScope.$apply(function(){
-						NotifyService.info("User "+$scope.userEmail+" added");
+						NotifyService.info("User "+$scope.user.email+" added");
 						//TODO: enhancement: append the item to the list.
 		            	$route.reload();
 					})
 				})
 				.catch(function(){
 					$rootScope.$apply(function(){
-						NotifyService.error("An error occurred while adding "+$scope.userEmail,$scope.add);
+						NotifyService.error("An error occurred while adding "+$scope.user.email);
 					})
 				});
 				
