2f13de538cfa246cc2aacd26a90fc177e5e02514
= <=>
2014-12-05 12:44:32 +0100

    md-datepicker updated

--- src/main/webapp/components/md-datepicker/md-datepicker.js
+++ src/main/webapp/components/md-datepicker/md-datepicker.js
@@ -14,12 +14,15 @@ function MdDatepickerDirective($mdUtil) {
 		require: ['^?mdInputGroup'],
 		scope: {
 			fid : '@mdFid',
+			format: '@',
+			minView: '@',
+			maxView: '@',
 			value: '='
 		},
 		template: 
 			'<div>'+
 			'<div ng-show="value" layout="row" style="border-bottom: 1px solid rgba(0, 0, 0, 0.12); padding-top:2px;">'+
-			'	<div class="datepicker-value" flex><span>{{value|date}}</span></div>'+
+			'	<div class="datepicker-value" flex><span>{{value|date:format}}</span></div>'+
 			'	<md-button ng-show="value" ng-click="value=null;focusTextbox()" style="padding:0;">'+
 			'	 	<md-icon icon="/img/icons/ic_cancel_24px.svg"></md-icon>'+
 			' 	</md-button>'+
@@ -28,7 +31,7 @@ function MdDatepickerDirective($mdUtil) {
 			'<div ng-show="focused" class="datepicker-container" ng-mouseenter="onMouseEnter()" ng-mouseleave="onMouseLeave()">'+
 			'	<md-whiteframe class="md-whiteframe-z1" layout>'+
 			'		<md-content>'+
-			'			<div date-picker="value"></div>'+
+			'			<div date-picker="value" min-view="{{minView}}" max-view="{{maxView}}"></div>'+
 			'		</md-content>'+
 			'	</md-whiteframe>'+
 			'</div>'+
