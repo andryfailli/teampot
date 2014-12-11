455a7d1ca5288296e2b7502194fd660069a472ef
= <=>
2014-12-11 14:43:28 +0100

    project list update on notification

--- src/main/webapp/components/project/list.js
+++ src/main/webapp/components/project/list.js
@@ -1,6 +1,12 @@
 angular.module('teampot').
-	controller('projectListController', function($rootScope,$scope,GapiClient,ProjectService) {
+	controller('projectListController', function($rootScope,$scope,GapiClient,ProjectService,RealtimeService) {
 		
 		$scope.entityList = ProjectService.$list();
+		
+		$scope.entityList = ProjectService.$list();
+		var watchHandle = RealtimeService.registerWatch("Project",function(){return ProjectService.$list();});
+		RealtimeService.subscribe("Project",$scope.entityList);
+		
+		$scope.$on("$destroy",function(){RealtimeService.unregisterWatch(watchHandle);});
 		
 	});
